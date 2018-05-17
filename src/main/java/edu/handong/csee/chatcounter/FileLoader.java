ce56a8c85310f6d718fc80cf5b29b768c15dfbbd
leehyunji0715@gmail.com <Leehyunji0715>
2018-05-18 01:25:15 +0900

    commit

--- src/main/java/edu/handong/csee/chatcounter/FileLoader.java
+++ src/main/java/edu/handong/csee/chatcounter/FileLoader.java
@@ -4,16 +4,46 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class FileLoader {
-	String fileName;
+	//String fileName="";
 	private ArrayList<String> messages;
-	public void readDirectory(String path) {
-		
+	private ArrayList<String> fileNames;
+	//final String fatternName = "자바";
+	String path = "/Users/leehyunji0715/Desktop/ChatCounter/";
+//	File file = new File(path);
+//	public void readDirectory(String path) {
+//	}
+	//private ArrayList<String> getFileNames(){
+	
+	private void getFileNames(){
+		File file = new File(path);
+		//ArrayList<String> fileNames = new ArrayList<String>();
+		fileNames = new ArrayList<String>();
+		String[] fList = file.list();
+		int i=0;
+		while(true) {
+			//fileNames.add(file.getName());
+			fileNames.add(fList[i]);
+			if(!file.exists()) {
+				break;
+			}
+			i++;
+		}
 	}
-	private ArrayList<File> getFileNames(){
-		
-		return 0;
-	}
-	public void getMessages() {
-		
+	public void getMessages(String fName) {
+		if(fName.contains(".txt")) { // 만약 txt파일이라
+			MessageWindow msgW = new MessageWindow();
+		}
+		else if(fName.contains(".csv")) {//csv 파일이라면...
+			MessageMac msgM = new MessageMac();
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/edu/handong/csee/chatcounter/Message.java
+++ src/main/java/edu/handong/csee/chatcounter/Message.java
@@ -1,5 +1,14 @@
 package edu.handong.csee.chatcounter;
 
-public class Message {
+import java.util.ArrayList;
 
+public class Message {
+	private ArrayList<String> mUser;
+	private ArrayList<String> mDate;
+}
+class MessageWindow extends Message{
+	private ArrayList<String> mMessages;
+}
+class MessageMac extends Message{
+	private ArrayList<String> mMessages;
 }
